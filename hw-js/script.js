c6d7f1e712e33605ae5fd779e443fd1f4d4fd25b
Mira <154324598+Miraamal@users.noreply.github.com>
2024-02-01 17:19:42 +0100

    Add files via upload

---
+++ hw-js/script.js
@@ -0,0 +1,21 @@
+/*const userInput = +prompt("Enter number")
+const result = userInput * 0.1
+console.log(`10% out of entered number is: ${result}`)
+
+
+/*const firstNumber = +prompt("Enter first number")
+const secondNumber = +prompt("Enter second number")
+const minimumNumber = (firstNumber < secondNumber) ? firstNumber : secondNumber
+console.log(`minimumnumber: ${minimumNumber}`)
+*/
+
+/*const userInput = +prompt("Enter number")
+const message = (userInput < 100) ? "Number is lower then 100" : (userInput > 100 ? "Number is higher then 100" : "Number is equal to 100")
+console.log(message)
+*/
+
+/*const userName = prompt("Enter your name")
+const userAge = prompt("Enter your age")
+const welcomeMessage = (userAge >= 18) ? `Hello, ${userName}!` : `Hi, ${userName}!`
+console.log(welcomeMessage)
+*/
